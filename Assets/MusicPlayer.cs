4b5e359c69e61c215ae251f759291622342674ac
Kyle Chadwick <kylepchad@gmail.com>
2017-12-22 21:36:05 -0800

    splash screen

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    [SerializeField] float levelLoadDelay = 2f;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject); //keep music going into scenes
+    }
+
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadFirstScene", levelLoadDelay);
+	}
+
+    private void LoadFirstScene() {
+        print("load first scene");
+        SceneManager.LoadScene(1);
+    }
+}
