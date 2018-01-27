77ea6fc27daf6be8dbe82d8ccc2fb3dab77af845
Kyle Chadwick <kylepchad@gmail.com>
2018-01-27 09:40:35 -0800

    horizontal movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 40f;
+    [Tooltip("In m")] [SerializeField] float xRange = 30f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,6 +17,8 @@ public class Player : MonoBehaviour {
 	void Update () {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
-        print(xOffset);
+        float rawXPos = transform.localPosition.x + xOffset;
+        float XPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        transform.localPosition = new Vector3(XPos, transform.localPosition.y, transform.localPosition.z);
 	}
 }
